7e22dbfa5c79899f409e16ae44aadba1978d3701
Vivek Mathew <vmathew@egen.solutions>
2017-02-01 21:04:07 -0600

    Adding the Seed

---
+++ src/main/java/com/neo/bristal/BristalSeedApplication.java
@@ -0,0 +1,12 @@
+package com.neo.bristal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BristalSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BristalSeedApplication.class, args);
+	}
+}
---
+++ src/test/java/com/neo/bristal/BristalSeedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neo.bristal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BristalSeedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
